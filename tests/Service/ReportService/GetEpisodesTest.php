c4b191c6d09d229a4b641ae97c5ef8ad63f8cb92
danik <d.shumakovich@gmail.com>
2025-03-21 12:13:17 +0100

    Update GetEpisodesTest.php based on testGetEpisodesWithJoin().

--- tests/Service/ReportService/GetEpisodesTest.php
+++ tests/Service/ReportService/GetEpisodesTest.php
@@ -31,13 +31,11 @@ class GetEpisodesTest extends TestCase
 
         $episodes = $this->reportService->getEpisodes($contents, CategoryType::OTHER);
 
-        $this->assertCount(5, $episodes);
+        $this->assertCount(3, $episodes);
 
         $this->assertEquals(CategoryType::OTHER, $episodes[0]->getCategory());
         $this->assertEquals(CategoryType::DANCE, $episodes[1]->getCategory());
-        $this->assertEquals(CategoryType::OTHER, $episodes[2]->getCategory());
-        $this->assertEquals(CategoryType::CEREMONY, $episodes[3]->getCategory());
-        $this->assertEquals(CategoryType::OTHER, $episodes[4]->getCategory());
+        $this->assertEquals(CategoryType::CEREMONY, $episodes[2]->getCategory());
     }
 
     public function testGetEpisodesWithCategory(): void
@@ -51,12 +49,10 @@ class GetEpisodesTest extends TestCase
 
         $episodes = $this->reportService->getEpisodes($contents, CategoryType::OTHER);
 
-        $this->assertCount(3, $episodes);
+        $this->assertCount(2, $episodes);
 
         $this->assertEquals(CategoryType::OTHER, $episodes[0]->getCategory());
-        $this->assertArrayNotHasKey(1, $episodes);
-        $this->assertEquals(CategoryType::SONGS, $episodes[2]->getCategory());
-        $this->assertEquals(CategoryType::SONGS, $episodes[3]->getCategory());
+        $this->assertEquals(CategoryType::SONGS, $episodes[1]->getCategory());
     }
 
     public function testGetEpisodesWithJoin(): void
@@ -74,8 +70,6 @@ class GetEpisodesTest extends TestCase
         $episodes = $this->reportService->getEpisodes($contents, CategoryType::STORY);
         $this->assertNotEmpty($episodes);
 
-        // todo   (use CategoryType::TEXT_JOIN)
-        /*
         $this->assertCount(2, $episodes);
 
         $this->assertEquals(CategoryType::OTHER, $episodes[0]->getCategory());
@@ -84,6 +78,6 @@ class GetEpisodesTest extends TestCase
 туряць
 слончык - малая лавачка
 падгалосьнік - той, хто падводзіць у песьні', $episodes[0]->getText());
-        */
+
     }
 }
